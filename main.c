0a4d00aa824caf8b5e958217ae482a0fa3a8d977
atlaspolat <apolat21@ku.edu.tr>
2024-12-07 20:06:33 +0300

    main.c added

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "mathutils.h"
+
+int main() {
+    int result = add(5, 3);
+    printf("5 + 3 = %d\n", result);
+
+    result = subtract(5, 3);
+    printf("5 - 3 = %d\n", result);
+
+    return 0;
+}
